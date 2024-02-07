08fcd6cbf0186be4e208c30a3bcd8b33ce6731f8
Chris Givens <givenscj@hotmail.com>
2024-02-07 11:08:30 -0500

    spektra updates

--- artifacts/environment-setup/spektra/post-install-script02.ps1
+++ artifacts/environment-setup/spektra/post-install-script02.ps1
@@ -148,19 +148,27 @@ $global:sqlPassword = $AzureSQLPassword          # READ FROM FILE
 $securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
 
-Connect-AzAccount -Identity
+#do the deployment...
+Connect-AzAccount -Credential $cred | Out-Null
 
-$resourceGroups = Get-AzResourceGroup
-$resourceGroup = $resourceGroups[0]
+# Template deployment
+$rg = (Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -like "*postgres" });
+$resourceGroupName = $rg.ResourceGroupName
+$region = $rg.Location;
+$deploymentId =  $rg.Tags["DeploymentId"]
+
+$sub = Get-AzSubscription;
+
+$subscriptionId = $sub.SubscriptionId;
 
 $servers = Get-AzPostgreSqlFlexibleServer
 $serverName = $servers[0].name
 
 $databaseName = "airbnb"
 
-New-AzPostgreSqlFlexibleServerDatabase -Name $databaseName -ResourceGroupName $resourceGroup.ResourceGroupName -ServerName $serverName
+New-AzPostgreSqlFlexibleServerDatabase -Name $databaseName -ResourceGroupName $ResourceGroupName -ServerName $serverName
 
-New-AzPostgreSqlFlexibleServerFirewallRule -FirewallRuleName $([Guid]::newguid().tostring())  -StartIpAddress '0.0.0.0' -EndIpAddress '0.0.0.0' -ServerName $serverName  -ResourceGroupName $resourceGroup.ResourceGroupName
+New-AzPostgreSqlFlexibleServerFirewallRule -FirewallRuleName $([Guid]::newguid().tostring())  -StartIpAddress '0.0.0.0' -EndIpAddress '0.0.0.0' -ServerName $serverName  -ResourceGroupName $ResourceGroupName
 
 # Template deployment
 $deploymentId =  (Get-AzResourceGroup -Name $resourceGroupName).Tags["DeploymentId"]
@@ -182,7 +190,7 @@ $filePath = "c:\labfiles\$workshopName\artifacts\data\airbnb.sql"
 $env:Path += ';C:\Program Files\PostgreSQL\16\bin'
 
 #set the password
-$env:PGPASSWORD="Seattle123Seattle123"
+$env:PGPASSWORD=$password
 psql -h "$($serverName).postgres.database.azure.com" -d $databaseName -U s2admin -p 5432 -a -w -f $filePath
 
 Stop-Transcript
